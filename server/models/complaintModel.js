983ba930e5f53dcc011ce425e8ac0679801e0052
sayyad Afsana <sayyadafsana2007@gmail.com>
2026-06-27 15:59:38 +0530

    add mongoose models

---
+++ server/models/complaintModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const complaintSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['open', 'in-progress', 'resolved'],
+    default: 'open'
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true
+  },
+  agent: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Agent'
+  }
+}, { timestamps: true })
+
+module.exports = mongoose.model('Complaint', complaintSchema)
